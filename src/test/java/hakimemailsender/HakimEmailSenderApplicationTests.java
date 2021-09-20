4a8602f4d0a5c0ecdeeae0a4756d79672e37ef0f
hodeiez <hodeinaiz@hotmail.com>
2021-09-20 10:15:52 +0200

    testing for github actions 3

--- src/test/java/hakimemailsender/HakimEmailSenderApplicationTests.java
+++ src/test/java/hakimemailsender/HakimEmailSenderApplicationTests.java
@@ -3,9 +3,11 @@ package hakimemailsender;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class HakimEmailSenderApplicationTests {
 
     @Test
