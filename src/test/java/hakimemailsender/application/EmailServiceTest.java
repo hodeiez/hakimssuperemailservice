b3258e72c32c6b0d105c71e0052d1fd5b5113130
hodeiez <hodeinaiz@hotmail.com>
2021-09-20 10:02:59 +0200

    testing for github actions

--- src/test/java/hakimemailsender/application/EmailServiceTest.java
+++ src/test/java/hakimemailsender/application/EmailServiceTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.server.ResponseStatusException;
@@ -22,6 +23,7 @@ import static org.mockito.Mockito.*;
 @SpringBootTest
 @AutoConfigureMockMvc
 @ContextConfiguration(classes = ApplicationConfiguration.class)
+@ActiveProfiles("test")
 public class EmailServiceTest {
 
     @Autowired
